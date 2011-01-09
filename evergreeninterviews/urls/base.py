4a7e94092e85dca0acdd345ab8e5248c426703d0
Matthew Scott <gldnspud@gmail.com>
2011-01-09 12:01:45 -0800

    Do not show individual redemptions, just the list.

--- evergreeninterviews/urls/base.py
+++ evergreeninterviews/urls/base.py
@@ -2,10 +2,20 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('',
+    # Direct the user to the coupon redeem page for now.
     url(regex=  u'^$',
         view=   'django.views.generic.simple.redirect_to',
         kwargs= dict(
             url='/coupons/redeem/',
+            permanent=False,
+        ),
+    ),
+    # Also, direct individual redemptions to the list of
+    # redemptions, to clean up the URL.
+    url(regex=  u'^coupons/redeemed/\d+/$',
+        view=   'django.views.generic.simple.redirect_to',
+        kwargs= dict(
+            url='/coupons/redeemed/',
             permanent=False,
         ),
     ),
