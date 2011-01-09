7611abb3552c3224c5d0c9e1aca33d0867401eae
Matthew Scott <gldnspud@gmail.com>
2011-01-09 12:41:48 -0800

    When conversion is done, direct them back to /.

--- evergreeninterviews/urls/base.py
+++ evergreeninterviews/urls/base.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('',
-    # Direct the user to the coupon redeem page for now.
+    # Direct the user to the coupons page.
     url(regex=  u'^$',
         view=   'django.views.generic.simple.redirect_to',
         kwargs= dict(
@@ -10,8 +10,16 @@ urlpatterns = patterns('',
             permanent=False,
         ),
     ),
-    # Also, direct individual redemptions to the list of
-    # redemptions, to clean up the URL.
+    # When conversion is done, direct back to the root URL.
+    url(name=   'lazysignup_convert_done',
+        regex=  u'^accounts/convert/done/$',
+        view=   'django.views.generic.simple.redirect_to',
+        kwargs= dict(
+            url='/',
+            permanent=False,
+        ),
+    ),
+    # Direct individual redemptions to the list of redemptions, to clean up the URL.
     url(regex=  u'^coupons/redeemed/\d+/$',
         view=   'django.views.generic.simple.redirect_to',
         kwargs= dict(
